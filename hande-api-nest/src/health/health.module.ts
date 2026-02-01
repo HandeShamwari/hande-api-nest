a3704ba128f150ad9af0898252d73da99180e60a
TafadzwaLawrence <chitangalawrence03@gmail.com>
2026-02-02 01:33:32 +0200

    Remove nested hande-api-nest folder

--- hande-api-nest/src/health/health.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { HealthController } from './health.controller';
-import { SharedModule } from '../shared/shared.module';
-
-@Module({
-  imports: [SharedModule],
-  controllers: [HealthController],
-})
-export class HealthModule {}
