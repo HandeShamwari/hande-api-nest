43d84df5cd23dc04e435a3dad56d1c21a82c35b4
TafadzwaLawrence <chitangalawrence03@gmail.com>
2026-02-02 10:38:13 +0200

    Fix: TripsModule imports JobsModule

--- src/trips/trips.module.ts
+++ src/trips/trips.module.ts
@@ -1,12 +1,13 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { TripsController } from './controllers/trips.controller';
 import { BidsController } from './controllers/bids.controller';
 import { TripsService } from './services/trips.service';
 import { BidsService } from './services/bids.service';
 import { SharedModule } from '../shared/shared.module';
+import { JobsModule } from '../jobs/jobs.module';
 
 @Module({
-  imports: [SharedModule],
+  imports: [SharedModule, forwardRef(() => JobsModule)],
   controllers: [TripsController, BidsController],
   providers: [TripsService, BidsService],
   exports: [TripsService, BidsService],
